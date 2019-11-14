5121e5da4500fa054d289adf54108021034ec90e
Shengyou Fan <shengyoufan@gmail.com>
2019-11-14 19:49:59 +0800

    install ContentNegotiation feature

--- src/Application.kt
+++ src/Application.kt
@@ -2,8 +2,11 @@ package io.kraftsman.ktor.api
 
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -13,6 +16,12 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    install(ContentNegotiation) {
+        jackson {
+
+        }
+    }
 
     routing {
         get("/") {
